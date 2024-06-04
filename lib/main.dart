781069b582cbf28bd5c502d1322c346c8c092c25
Anselmo Lucas <anselmo@miketec.com.br>
2024-06-04 13:23:49 -0300

    estratégia multprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/counter_page.dart';
@@ -20,18 +21,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => new ProductList(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          fontFamily: 'Lato',
-          hintColor: Colors.deepOrange,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => ProductList(),            
+            ),
+          ChangeNotifierProvider(
+            create: (_) => Cart(),            
+            ),
+        ],        
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            fontFamily: 'Lato',
+            hintColor: Colors.deepOrange,
+          ),
+          home: ProductsOverviewPage(),
+          routes:{
+            AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+          }
         ),
-        home: ProductsOverviewPage(),
-        routes:{
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
-        }
       ),
     );
   }
