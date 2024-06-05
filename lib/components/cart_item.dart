400406dc73132188f4be13cbfb47c37955dc06fb
Anselmo Lucas <anselmo@miketec.com.br>
2024-06-05 12:20:29 -0300

    implementado o alertDialog para confirmar a exclusão de itens do carrinho

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -27,6 +27,23 @@ class CartItemWidget extends StatelessWidget {
           vertical: 4,
         ),
       ),
+      confirmDismiss: (_) {
+        return showDialog<bool>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Tem certeza?'),
+            content: Text('Quer remover o item do carrinho?'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(ctx).pop(false),
+                  child: Text('Não')),
+              TextButton(
+                  onPressed: () => Navigator.of(ctx).pop(true),
+                  child: Text('Sim')),
+            ],
+          ),
+        );
+      },
       onDismissed: (_) {
         Provider.of<Cart>(context, listen: false)
             .removeItem(cartItem.productId);
